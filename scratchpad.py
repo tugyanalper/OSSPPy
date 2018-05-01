0495c48a684401d2acf9c52ce19e11fc786ed18e
tugyanalper <tugyanalper@gmail.com>
2018-05-01 12:10:21 +0300

    Openshop Problemleri indirildi ve makespan yazıldı. Makespan plotlayan betik yazıldı.

--- scratchpad.py
+++ scratchpad.py
@@ -1,19 +1,9 @@
 from __future__ import print_function, division
 
-import array
 import random
 import sys
 
 import matplotlib.pyplot as plt
-import numpy as np
-from deap import base
-from deap import creator
-from deap import tools
-
-NGEN = 1000
-NPOP = 100
-CXPB = 0.9
-MUTPB = 0.01
 
 
 # Read processing times from the file
@@ -58,22 +48,12 @@ numberOfMachines = len(processing_times[0])
 NDIM = numberOfMachines * numberOfJobs
 # print(processing_times)
 
-operation_numbers_dictionary = {i: (i % numberOfMachines, i // numberOfMachines) for i in range(NDIM)}
-# print(operation_numbers_dictionary)
-
-# define the problem as an Minimization or Maximization by defining the weights
-creator.create("FitnessMin", base.Fitness, weights=(-1.0,))
-
-# create a container to hold individuals which have an attribute named "fitness"
-creator.create("Individual", array.array, typecode='B', fitness=creator.FitnessMin)
-
-toolbox = base.Toolbox()  # get toolbox class from base module of deap
-toolbox.register("attribute", random.sample, range(NDIM), NDIM)  # register attribute method to toolbox
-toolbox.register("individual", tools.initIterate, creator.Individual, toolbox.attribute)  # register individual method
-toolbox.register("population", tools.initRepeat, list, toolbox.individual)  # register population method to toolbox
+operation_numbers_dictionary = {i: (i % numberOfMachines, i // numberOfMachines)
+                                for i in range(NDIM)}
 
 
-def gannt_chart(schedule):
+# Define Objective Function
+def makespan(schedule):
     """Compiles a scheduling on the machines given a permutation of jobs"""
 
     flag_print = False
@@ -146,27 +126,14 @@ def gannt_chart(schedule):
             print('Machine Number is : {}'.format(machine_number))
             print('Job Number is : {}'.format(job_number))
             print("Processing time for job {} on machine {} : ".format(job_number, machine_number), proc_time)
-
-    return gantt_chart
-
-
-# Define Objective Function
-def makespan(schedule):
-    gannt_chrt = gannt_chart(schedule)
     ctimes = []
     for machine in range(numberOfMachines):
-        ctimes.append(gannt_chrt[machine][-1][-1])
+        ctimes.append(gantt_chart[machine][-1][-1])
     make_span = max(ctimes)
-    return make_span,  # return a tuple for compatibility
+    return gantt_chart, make_span  # return a tuple for compatibility
 
 
-def plot_gannt(machine_times, ms):
-    """
-    Plots the gannt chart of the given gannt chart data structure
-    :param machine_times: gannt chart data structure
-    :param ms: makespan value
-    :return: None 
-    """
+def plot_gannt(machine_times, make_span):
     fig, ax = plt.subplots()
     facecolors = ('blue', 'red', 'yellow', 'green', 'grey', 'azure', 'plum',
                   'wheat', 'brown', 'chocolate', 'coral', 'cyan', 'darkblue',
@@ -183,7 +150,7 @@ def plot_gannt(machine_times, ms):
         bar_start += increment
 
     ax.set_ylim(5, 65)
-    ax.set_xlim(0, ms)
+    ax.set_xlim(0, make_span)
     ax.set_yticks([15, 25, 35, 45, 55])
     yticklabels = ['Machine ' + str(i + 1) for i in range(numberOfMachines)]
     ax.set_yticklabels(yticklabels)
@@ -191,113 +158,17 @@ def plot_gannt(machine_times, ms):
     plt.show()
 
 
-def swap(individual):
-    idx1, idx2 = random.sample(range(NDIM), 2)
-    a, b = individual.index(idx1), individual.index(idx2)
-    individual[b], individual[a] = individual[a], individual[b]
-    return individual
-
-
-def evolve(pop):
-    # Select the next generation individuals
-    offspring = toolbox.select(pop, len(pop))
-
-    # Clone the selected individuals
-    offspring = list(map(toolbox.clone, offspring))  # clone offsprings
-
-    # Apply crossover on the offspring
-    for child1, child2 in zip(offspring[::2], offspring[1::2]):
-        if random.random() < CXPB:
-            toolbox.mate(child1, child2)
-            del child1.fitness.values
-            del child2.fitness.values
-
-    # Apply mutation on the offspring
-    for mutant in offspring:
-        if random.random() < MUTPB:
-            toolbox.mutate(mutant)
-            del mutant.fitness.values
-    # Evaluate the individuals with an invalid fitness
-    invalid_ind = [ind for ind in offspring if not ind.fitness.valid]
-    fitnesses = map(toolbox.evaluate, invalid_ind)
-    for ind, fit in zip(invalid_ind, fitnesses):
-        ind.fitness.values = fit
-    return offspring
-
-
-# register objective function with the "evaluate" alias to the toolbox
-toolbox.register("evaluate", makespan)
-
-# register crossover function with the "mate" alias to the toolbox
-toolbox.register("mate", tools.cxOrdered)
-
-# register mutation function with the "mutate" alias to the toolbox
-toolbox.register("mutate", swap)
-
-# register selection function with the "select" alias to the toolbox
-toolbox.register("select", tools.selTournament, tournsize=2)
-
-
 def main():
-    # random.seed(169)
-    pop = toolbox.population(n=NPOP)
-    hof = tools.HallOfFame(1)
+    # random.seed(1250)
+    schedule = random.sample(range(NDIM), NDIM)
+    # print(schedule)
+    machine_times, make_span = makespan(schedule)
+    # print(len(machine_times[0]))
+    for machine in range(numberOfMachines):
+        print("Gant Chart for Machine {}".format(machine + 1))
+        print(machine_times[machine])
+    print(make_span)
+    plot_gannt(machine_times, make_span)
 
-    stats = tools.Statistics(key=lambda ind: ind.fitness.values)
-    stats.register("avg", np.mean)
-    stats.register("std", np.std)
-    stats.register("min", np.min)
-    stats.register("max", np.max)
-
-    logbook = tools.Logbook()
-
-    # assign logbook headers
-    logbook.header = "gen", "evals", "min", "max", "avg", "std"
-
-    # populasyondaki her bir bireyin degerini hesapla ve
-    # o bireyin fitness degerini guncelle
-    fitnesses = map(toolbox.evaluate, pop)
-    for ind, fit in zip(pop, fitnesses):
-        ind.fitness.values = fit
-
-    record = stats.compile(pop)
-    logbook.record(gen=0, evals=len(pop), **record)
-    print(logbook.stream)
-    hof.update(pop)
-
-    cbestfitness = hof.items[0].fitness.values[0]
-
-    boolidx = np.empty((NGEN,), dtype=np.bool_)  # create an empty bool array to hold progress flags
-
-    for gen in range(1, NGEN):
-        offspring = evolve(pop)
-        pop[:] = tools.selBest(pop, 1) + tools.selBest(offspring, NPOP - 1) # use elitism
-        # pop[:] = offspring  # replace population with new offspring
-
-        hof.update(pop)
-
-        record = stats.compile(pop)
-        logbook.record(gen=gen, evals=len(pop), **record)
-        print(logbook.stream)
-
-        # if the best individual in new population has a better value than
-        # the previous best then update the current best fitness and mark the
-        # progress
-        if hof.items[0].fitness.values[0] < cbestfitness:
-            cbestfitness = hof.items[0].fitness.values[0]
-            boolidx[gen] = True  # mark the generation as a progress
-        else:
-            boolidx[gen] = False  # no progress
-
-    print("-- End of (successful) evolution --")
-
-    gen, evals, avg, minval, std = logbook.select('gen', 'evals', 'avg', 'min', 'std')
-    print(hof.items[0], " ", hof[0].fitness.values[0])
-    best_schedule = hof.items[0]
-    best_makespan = hof.items[0].fitness.values[0]
-    best_gannt_chart = gannt_chart(best_schedule)
-    plot_gannt(best_gannt_chart, best_makespan)
-
-
-if __name__ == "__main__":
+if __name__ == '__main__':
     main()
